cbb320cc99f03ae9134000c85a40d94da5acf4e0
BigFang <dafang2021@gmail.com>
2020-09-09 22:51:30 +0800

    添加找回vmess脚本

---
+++ vmess.sh
@@ -0,0 +1,16 @@
+ibmcloud target --cf && read -p "输入你的app名称：" appname&&path=$(ibmcloud cf ssh ${appname} -c "cat app/v2ray/config.json"|grep path|sed -e 's/ //g'|sed -e 's/\path//g'|sed -e 's/\"//g'|sed -e 's/\://g'|sed -e 's/\,//g')&&id=$(ibmcloud cf ssh ${appname} -c "cat app/v2ray/config.json" |grep id|sed -e 's/ //g'|sed -e 's/\id//g'|sed -e 's/\"//g'|sed -e 's/\://g'|sed -e 's/\,//g')&&VMESSCODE=$(base64 -w 0 << EOF
+  {
+     "v": "2",
+     "ps": "ibmyes",
+     "add": "${appname}.us-south.cf.appdomain.cloud",
+     "port": "443",
+     "id": "${id}",
+     "aid": "4",
+     "net": "ws",
+     "type": "none",
+     "host": "",
+     "path": "${path}",
+     "tls": "tls"
+  }
+EOF
+)&&echo "配置链接："&&echo vmess://${VMESSCODE}
