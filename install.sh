4275a58e3139062867dbfecf83a7a57d4ee45de3
BigFang <dafang2021@gmail.com>
2020-09-10 21:37:21 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -36,7 +36,7 @@ ibmcloud cf push
 domain=`ibmcloud cf app $appname | grep routes | cut -f2 -d':' | sed 's/ //g'`
 vmess=`echo '{"add":"'$domain'","aid":"64","host":"","id":"'$uuid'","net":"ws","path":"/'$path'","port":"443","ps":"IBMVPS","tls":"tls","type":"none","v":"2"}' | base64 -w 0`
 cd ..
-echo 容器已经成功启动
+echo 恭喜你已经配置成功
 echo 地址: $domain
 echo UUID: $uuid
 echo path: /$path
